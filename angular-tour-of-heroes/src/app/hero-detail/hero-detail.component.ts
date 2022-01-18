b2005576c5f3f68cfd9e99620ee7c52a6e5dd040
PoMaksBo <1994Deterok@mail.ru>
2022-01-18 17:00:48 +0500

    modificated app, add HTTP

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -31,4 +31,10 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    // @ts-ignore
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
